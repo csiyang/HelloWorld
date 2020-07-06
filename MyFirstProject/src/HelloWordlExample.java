82de3255fdc722af318036f58afc2d2addfe731f
csiyang <67619008+csiyang@users.noreply.github.com>
2020-07-06 13:35:25 +1200

    Adding in more description
    Add description of what the expected out come of code is

--- MyFirstProject/src/HelloWordlExample.java
+++ MyFirstProject/src/HelloWordlExample.java
@@ -1,10 +1,8 @@
-
 public class HelloWordlExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Output Hello World to the user
 		System.out.println("Hellow World");
-
 	}
 
 }
